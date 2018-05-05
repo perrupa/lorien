ba48073d0b408edbc68ccc287693836b78a8d180
Chris Marlow <christopher.marlow@shopify.com>
2018-05-04 20:36:33 -0400

    new header with leaves

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,10 +3,8 @@ import './header.scss'
 
 export default ({ children = [] }) => (
   <header className="header">
-    <h1 className='header__title'>
-      Lorien Masters
-    </h1>
-    <img className="header__headshot" src="images/Lo.jpg" alt="Beautiful young life coach" />
-    <em className='header__subtitle'>Transformation Life Coach</em>
+    <img className="header__image"
+      src="images/header.png"
+      alt="Beautiful young life coach" />
   </header>
 )
