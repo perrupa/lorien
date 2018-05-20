70af9456f56c3f80dba2066a93c1cf9f3406015f
Chris Marlow <christopher.marlow@shopify.com>
2018-05-20 16:25:06 -0400

    Better titles

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -4,10 +4,10 @@ import Link from 'gatsby-link'
 const AboutMe = () =>
   <article className='about-me'>
     <h2>Who I am</h2>
+
     <p> I am a Transformational Life Coach here to support and empower women to find self-love and rediscover themselves. I am an expert in overcoming the low, thriving on the highs, and  rebuilding  confidence, and a sense of self. With you,  we will unlock  the  hidden thought patterns holding you back in life and leaving you burnt out and uninspired.  You will be empowered  to feel a stronger connection to yourself, so you can regain the excitement and  self-commitment that  makes life wonderful </p>
-    <div>
-      <Link to="/who-i-am">Read more...</Link>
-    </div>
+
+    <p><Link to="/who-i-am">Read more...</Link></p>
   </article>
 
 export default AboutMe
