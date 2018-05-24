b453d28d485adbf8fe815ede6cc7d607eae923c8
LoMasters <39573565+LoMasters@users.noreply.github.com>
2018-05-23 20:52:40 -0400

    Updated newsletter wording

--- src/components/newsletter-cta.js
+++ src/components/newsletter-cta.js
@@ -4,7 +4,9 @@ import SubscribeLink from 'components/subscribe-button'
 const NewsletterCTA = () =>
   <article>
     <h3>Inspiring E-mails</h3>
-    <p>Join my email list to receive weekly motivational tips as well as inspirational quotes and videos. By becoming a member you will also receive a sneak peak to my newest blogs, along with an invite to my join my exclusive Facebook Group. In this group you will be welcomed by a community of women, where I will share new ways to empower yourself and techniques to rediscover your inner voice. Can’t wait to connect.</p>
+    <p>
+    Join my email list to become an exclusive member of my tribe. Within this group you will receive weekly motivational tips as well as inspirational quotes and videos, along with being welcomed by a community of amazing women. Here I will share ways to empower yourself and techniques to rediscover your inner voice. Can’t wait to connect.   
+    </p>
     <center>
       <p>
         <SubscribeLink>Subscribe to my E-mail list</SubscribeLink>
