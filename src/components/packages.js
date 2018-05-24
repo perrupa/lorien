d0d5ca20cf364ede45f31ffbb8c8b0f9f2b62700
Chris Marlow <christopher.marlow@shopify.com>
2018-05-23 23:08:02 -0400

    adding react-markdown and heading tweaks

--- src/components/packages.js
+++ src/components/packages.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
 import Card from 'components/card'
 import './packages.scss'
+
+const text = `
+## Work wi th me
+
+The following programs are ...
+
+Designed for those that are ready to;
+
+- Truly commit to life, and experience a restorative transformation
+- Realize your ability to manifest and unlock your purpose
+- Create an extraordinary life, that is aligned with your authentic self
+- Transform & re-discover the life you were meant to live
+- Live a life of self love, joy, passion, and purpose
+- Follow your intuition, and be open to what the universe presents you
+- Be grateful for the life you have, and willing to devote your time into creating an awakening future for your heart and soul.
+
+NOT for you if you:
+
+- Are not ready or willing to invest in yourself
+- Won’t to let go of excuses, and take ownership for your own life and choices
+- Are not going to show up 100% and commit
+- Don’t know your WHY
+- Aren’t able to hold yourself accountable for your journey
+`
 
 const Packages = () =>
   <div>
     <div className="package-intro">
       <article className="package-intro">
-        <h2>Work wi&nbsp;th me</h2>
-        <div>
-          <p> The following programs are NOT for you if: </p>
-
-          <ul>
-              <li>Your not ready or willing to put the work in</li>
-              <li>You you aren’t ready to make the investment for yourself</li>
-              <li>You won’t show up 100%</li>
-              <li>You don’t know your WHY</li>
-              <li>You aren’t able to hold yourself accountable for your journey</li>
-          </ul>
-
-          <p> Designed for those that are ready: </p>
-
-
-          <ul>
-            <li>To truly commit to life & experience a restorative transformation</li>
-            <li>To realize & manifest your calling & unlock the purpose that is within you</li>
-            <li>To create & live a life that is aligned with your authentic self</li>
-            <li>To transform & re-discover the life you were meant to live</li>
-            <li>To live a life of self love, joy, passion, purpose & meaning</li>
-
-            <li>To ignite the spark within & live a more inspired & mindful life</li>
-            <li>To follow your intuition, & be open to what the universe is trying to give you</li>
-            <li>To be grateful of the life you have, and willing to devote the time and effort into creating a new future.</li>
-          </ul>
-        </div>
+        <ReactMarkdown source={text} />
       </article>
     </div>
 
