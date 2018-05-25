29170c2368fbf586efa6b23c64fd791a86d94f46
Chris Marlow <christopher.marlow@shopify.com>
2018-05-24 23:00:05 -0400

    fixing the footer and cleaning up styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,21 +4,28 @@ import twitter from 'static/icons/twitter.svg'
 import insta from 'static/icons/insta.svg'
 import './footer.scss'
 
+const Image = ({src, alt}) =>
+  <img className="footer__icon" src={src} alt={alt} />
+
 const Footer = () =>
   <div className="footer">
     <div className="icon-container">
       <a className="link" href="http://instagram.com/lorienmasters">
-        <img src={insta} alt="follow me on Instagram" />
+        <Image src={insta} alt="follow me on Instagram" />
       </a>
+
       <a className="link" href="https://facebook.com/lorien.alexandra">
-        <img src={fb} alt="follow me on Facebook" />
+        <Image src={fb} alt="follow me on Facebook" />
       </a>
+
       <a className="link" href="https://twitter.com/lolifecoach">
-        <img src={twitter} alt="follow me on Twitter" />
+        <Image src={twitter} alt="follow me on Twitter" />
       </a>
     </div>
 
-    <p>© Lorien Masters 2018</p>
+    <p className="footer__text">
+      © Lorien Masters {new Date().getFullYear()}
+    </p>
   </div>
 
 export default Footer
