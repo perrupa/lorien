36079c24d095fcc6cd93e666e0784da4f6dca64b
Chris Marlow <christopher.marlow@shopify.com>
2018-09-14 19:23:38 -0400

    fixing problem with list

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,15 +7,15 @@ const Intro = () =>
       <img className="intro__backup-photo" src="../../static/images/Lo.jpg" alt="" />
     </div>
 
-    <p className="intro__content">
+    <div className="intro__content">
         {`I'm Lo, a transformational life coach. My mission is to guide and empower
-      you to find love with your most authentic self. Together we will build a healthy 
+      you to find love with your most authentic self. Together we will build a healthy
       sense of self-esteem and self-love by`}
       <ul>
         <li>Sharing your story and learning you’re not alone</li>
         <li>Finding out what makes you special and nurturing it</li>
       </ul>
-    </p>
+    </div>
   </article>
 
 export default Intro
